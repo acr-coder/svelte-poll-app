11511057a07a5ae6eef41d090727e2eaec5a93c3
acr-coder <acarerisa1@gmail.com>
2022-02-20 09:53:10 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+	import Header from './components/Header.svelte'
+	import Footer from './components/Footer.svelte'
+	import PollList from './components/PollList.svelte'
+	import Tabs from './shared/Tabs.svelte'
+	import CreatePollForm from './components/CreatePollForm.svelte'
+
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	}
+
+	let polls = [
+		{
+			id:1,
+			question: 'Python or Javascript?',
+			answerA: 'Python',
+			answerB: 'Javascript',
+			votesA:9,
+			votesB:15,
+		},
+	]
+
+	const handleAdd = (e) => {
+		const poll = e.detail;
+		polls = [poll, ...polls];
+		console.log(polls);
+		activeItem = 'Current Polls'
+	}
+</script>
+
+<Header />
+<main>
+	<Tabs {items} {activeItem} on:tabChange={tabChange} />
+	{#if activeItem === 'Current Polls' }
+		<PollList {polls}  />
+	{:else if activeItem === 'Add New Poll' }
+	<CreatePollForm on:add={handleAdd} />
+	{/if}
+</main>
+<Footer />
+
+<style>
+	main{
+		max-width: 960px;
+		margin: 40px auto;
+	}
+	
+</style>
